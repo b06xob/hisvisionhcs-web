f0640eac47860f0eea54852e5865683fc614d414
His Vision Healthcare Services <hisvisionhealthcare@gmail.com>
2025-10-16 22:55:52 -0400

    Initial commit - His Vision Healthcare Services website

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HisVisionHCS.Web.Models;
+
+namespace HisVisionHCS.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        ViewData["Title"] = "About Us";
+        return View();
+    }
+
+    public IActionResult Services()
+    {
+        ViewData["Title"] = "Our Services";
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        ViewData["Title"] = "Contact Us";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        ViewData["Title"] = "Privacy Policy";
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
